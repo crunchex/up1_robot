4f8ed0061f8d084c9a8b5edb9b36fb3df824ad11
Mike Lewis <mike@updroid.com>
2015-09-29 15:19:21 -0700

    old_stuff: moved into a separate backup repo

--- old_stuff/up1_perception/scripts/calibrate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.025 right:=/stereo/right/image_raw left:=/stereo/left/image_raw left_camera:=/stereo/left right_camera:=/stereo/right --approximate=0.05
--- old_stuff/up1_perception/scripts/convert.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rosrun camera_calibration_parsers convert /home/crunchex/uproot/catkin_ws/src/updroid_api/up1_perception/config/stereo_right.ini /home/crunchex/uproot/catkin_ws/src/updroid_api/up1_perception/config/stereo_right.yaml;
-rosrun camera_calibration_parsers convert /home/crunchex/uproot/catkin_ws/src/updroid_api/up1_perception/config/stereo_left.ini /home/crunchex/uproot/catkin_ws/src/updroid_api/up1_perception/config/stereo_left.yaml;
